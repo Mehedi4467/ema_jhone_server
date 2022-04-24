b7e68f30728bdb7472b8f84ec801fa5e9bd595f5
Mehedi4467 <mehedihassan4467@gmail.comm>
2022-04-25 03:15:41 +0600

    database connected

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
@@ -8,6 +10,27 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+
+// connect mongo db
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9x7m2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db("emaJhoneDB").collection("products");
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 
 
